0418dac767a49d6c2a682b62fa27000363d8dc54
sudheerj <jsudheer50@gmail.com>
2020-12-01 19:30:08 +0530

    Update allure report configuration

--- tests/e2e/jenkins/run-tests.sh
+++ tests/e2e/jenkins/run-tests.sh
@@ -3,7 +3,12 @@
 VERSION=${1:-latest}
 
 echo "Pulling codecept/e2e image: ${VERSION}"
+mkdir report
 
 docker run --rm \
     -v "${pwd}"/report/:/workspace/report/ \
     codecept/e2e:${VERSION}
+
+status= $?
+echo "Final status ${status}"
+exit ${status}
